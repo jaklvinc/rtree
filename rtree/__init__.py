464f2c65f4b2ce5540fb3ecfe93609b209e90e2f
Vincent Jakl <jaklvinc@fit.cvut.cz>
2021-05-12 18:40:41 +0200

    Started to implement insertion onto the tree

--- rtree/__init__.py
+++ rtree/__init__.py
@@ -1,36 +1,102 @@
 from enum import Enum
+import itertools
 from rtree.storage import RTreeStorage
 from rtree.node import Entry, Node
 from queue import Queue
-
+from itertools import combinations
 
 class RTreeSplitType(Enum):
-    TYPE1 = 1,
-    TYPE2 = 2,
-    TYPE3 = 3
+    BRUTE_FORCE = 1,
+    QUADRATIC = 2,
+    LINEAR = 3
 
 
 class RTree:
     def __init__(self, storage: RTreeStorage):
         self._storage = storage
-    
-    def _split_node(self):
-        pass
-    
-    def _add_to_node(self, node : Node ):
-        pass
-    
-    def insert(self, idx : tuple() , data : int ):
-        root_node=self._storage.get_node(0)
-        new_entry=Entry(first_pos = idx, data = data )
 
-        if ( root_node.is_leaf() ):
-            if ( not root_node.is_full() ):
-                pass
+    def _count_area( first : Entry , second : Entry ) -> int:
+        first_coords=list()
+        second_coords=list()
+        for x in range(first.get_first_pos().count()):
+            first_coords.append(min(first.get_first_pos()[x],second.get_first_pos()[x]))
+            second_coords.append(max(first.get_second_pos()[x],second.get_second_pos()[x]))
+        sizes=( abs(x-y)+1 for x in first_coords for y in second_coords )
+        area=1
+        for size in sizes:
+            area*=size
+        return area
+    
+    def _choose_leaf(self, node_idx : int , pos : tuple(int) ) -> Node:
+        node_queue = Queue(0)
+        node_queue.put(node_idx)
+
+        while ( not node_queue.empty() ):
+            this_node_idx=node_queue.get()
+            node = self._storage.get_node(this_node_idx)
+            node.set_idx(this_node_idx)
+            if ( node.is_leaf() ):
+                return node
+            else:
+                min_enlarge = float('inf')
+                min_entry = Entry
+                for entry in node.entries:
+                    enlarge_count = 0
+                    #Checks how much the Node would enlargen, if the new item would be added to this node  
+                    for x in range(entry.get_first_pos().count()):
+                        enlarge_count+=self._add_if_bigger(entry.get_first_pos()[x],pos[x])
+                        enlarge_count+=self._add_if_bigger(pos[x],entry.get_second_pos()[x])
+                    
+                    if ( enlarge_count < min_enlarge ):
+                        min_enlarge=enlarge_count
+                        min_entry=entry
+                    
+                    #for better size optimalization, if the hypothetical enlargements are the same, chooses the smaller area,
+                    # so that the tree is balanced better  
+                    elif ( enlarge_count == min_enlarge ):
+                        this_size=0
+                        last_min_size=0
+                        for x in range(entry.get_first_pos().count()):
+                            this_size+=(entry.get_second_pos()[x]-entry.get_first_pos[x])
+                            last_min_size+=(min_entry.get_second_pos()[x]-min_entry.get_first_pos()[x])
+                        if ( this_size < last_min_size ):
+                            min_enlarge=enlarge_count
+                            min_entry=entry
+                node_queue.put(min_entry.get_child_idx())
+
+    def _brute_force_split(self, split_this : Node ):
+        #TODO
+        pass
+
+    def _quadratic_split(self, split_this : Node ):
+        max_area = float('inf')
+        max_area_pair = 0
+        for pair in combinations(split_this.entries,2):
+            area=self._count_area(pair[0],pair[1])
+        #TODO
+
+    def _linear_split(self, split_this : Node ):
+        pass
+
+    def _split_node( self, split_this : Node , insert_node : RTreeSplitType ):
+        if ( insert_node == RTreeSplitType.BRUTE_FORCE ):
+            self._brute_force_split(split_this)
+        elif( insert_node == RTreeSplitType.QUADRATIC ):
+            self._quadratic_split(split_this)
+        elif( insert_node == RTreeSplitType.LINEAR ):
+            self._linear_split(split_this)
+        pass
+
+    def insert(self, idx : tuple(int) , data : int ):
+        to_insert = Entry(first_pos=idx,data=data)
+        insert_node=self._choose_leaf( 0 , idx )
+        if ( insert_node.add_entry( to_insert ) ):
+            self._storage.set_node(insert_node.get_idx(),insert_node)
         else:
-            pass
+            self._split_node(insert_node,self._storage.get_split_type())
+        #TODO
 
-    def find(self , search_pos : tuple() , node_idx : int = 0 ) -> int :
+    def find(self , search_pos : tuple(int) , node_idx : int = 0 ) -> int :
         node_queue = Queue(0)
         node_queue.put(self._storage.get_node(node_idx))
 
@@ -56,4 +122,5 @@ class RTree:
         return -1
 
     def find_in_range(self):
+        #TODO
         pass
--- rtree/node.py
+++ rtree/node.py
@@ -4,7 +4,7 @@ from abc import abstractclassmethod
 class Entry:
     '''Class that stores both leaf and non-leaf entries
     
-    For leaf entry, fill @first_pos and @data. \\
+    For leaf entry, fill @first_pos=@second_pos and @data. \\
     For non-leaf entry, fill @first_pos, @second_pos and @child_idx.
     '''
     def __init__(self,first_pos : tuple() , second_pos : tuple() , child_idx : int = -1 , data : int = -1 ) -> None :
@@ -25,13 +25,28 @@ class Entry:
     def get_second_pos(self) -> tuple() :
         return self._second_pos
 
+'''
 
+@parent is a pair of parent_node and index of parent_entry in that node
+'''
 class Node:
-    def __init__(self, is_leaf : bool , max_size : int ):
+    def __init__(self, is_leaf : bool , max_size : int , parent : tuple(int) ):
         self._is_leaf = is_leaf
         self._max_size = max_size
+        self._mem_idx = -1
+        self._parent_entry = parent
 
         self.entries = list(Entry)
+    
+    def get_parent_entry(self) -> tuple(int):
+        return self._parent_entry
+    
+    def set_idx(self,  mem_idx : int ) -> None:
+        self._mem_idx=mem_idx
+        pass
+    
+    def get_idx(self) -> int:
+        return self._mem_idx
     
     def is_leaf(self) -> bool:
         return self._is_leaf
@@ -39,6 +54,9 @@ class Node:
     def is_full(self) -> bool:
         return (self._entries.count() == self._max_size )
     
-    def add_entry(self, entry: Entry) -> None:
-        
-        pass
+    def add_entry(self, entry: Entry) -> bool:
+        if ( self.entries.count() == self._max_size ):
+            return False
+        else:
+            self.entries.append(entry)
+            return True
