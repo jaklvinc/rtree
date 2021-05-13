26b06274e87df75c7619ba76551f18aac1ca2c35
Matouš Kulhan <kulhama7@fit.cvut.cz>
2021-05-13 07:21:32 +0200

    Update RTreeSplitType

--- rtree/split_type.py
+++ rtree/split_type.py
@@ -2,6 +2,19 @@ from enum import Enum
 
 
 class RTreeSplitType(Enum):
-    BRUTE_FORCE = 1,
-    QUADRATIC = 2,
+    BRUTE_FORCE = 1
+    QUADRATIC = 2
     LINEAR = 3
+
+    @classmethod
+    def from_str(cls, value: str):
+        if value == 'bruteforce':
+            return cls.BRUTE_FORCE
+
+        if value == 'quadratic':
+            return cls.QUADRATIC
+
+        if value == 'linear':
+            return cls.LINEAR
+
+        return None
