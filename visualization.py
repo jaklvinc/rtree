1a0d0e581081693e2d80ca6af357a8018907b937
Matouš Kulhan <kulhama7@fit.cvut.cz>
2021-05-15 07:21:34 +0200

    Change visualization

--- visualization.py
+++ visualization.py
@@ -1,14 +1,12 @@
 import plotly.graph_objects as go
 from typing import List
 from collections import deque
-from rtree import RTree, RTreeSplitType
-from rtree.node import Node, NonLeafEntry, LeafEntry
-import random
+from rtree import RTree
 
 
 class Visualizer:
     @classmethod
-    def visualize(cls, tree: RTree):
+    def visualize(cls, tree: RTree, html: bool = False):
         dim = tree._storage.get_dim()
 
         if dim < 1 or dim > 3:
@@ -18,54 +16,57 @@ class Visualizer:
             return
 
         queue = deque()
-        queue.append(tree._storage.get_node(0))
+        queue.append((0, tree._storage.get_node(0)))
 
         data = [[] for _ in range(dim)]
         fig = go.Figure()
 
         while queue:
-            node = queue.popleft()
+            i, node = queue.popleft()
             if node.is_leaf():
                 for le in node.entries:
                     for i, x in enumerate(le.coord):
                         data[i].append(x)
             else:
                 for nle in node.entries:
-                    queue.append(tree._storage.get_node(nle.child_idx))
+                    queue.append((nle.child_idx, tree._storage.get_node(nle.child_idx)))
                     box = cls._box(nle.first_coord, nle.second_coord, dim)
-                    box.name = 'Node {}'.format(nle.child_idx)
+                    box.name = 'R{}(parent R{})'.format(nle.child_idx, i)
                     fig.add_trace(box)
         fig.add_trace(cls._data(data, dim))
-        #fig.show()
-        fig.write_html("HTML.html", auto_open=True)
+
+        if html:
+            fig.write_html("./vis.html", auto_open=True)
+        else:
+            fig.show()
 
     @classmethod
-    def _box(cls, p1: List[int], p2: List[int], dim: int):
+    def _box(cls, p: List[int], q: List[int], dim: int):
         if dim == 1:
-            return cls._box1d(p1, p2)
+            return cls._box1d(p, q)
         if dim == 2:
-            return cls._box2d(p1, p2)
-        return cls._box3d(p1, p2)
+            return cls._box2d(p, q)
+        return cls._box3d(p, q)
 
     @classmethod
     def _data(cls, data: List[List[int]], dim: int):
         if dim == 1:
-            return go.Scatter(x=data[0], y=[0 for _ in data[0]], name='Data', mode='markers')
+            return go.Scatter(x=data[0], y=[0 for _ in data[0]], name='Data', mode='markers', marker={'color': '#000000'})
         if dim == 2:
-            return go.Scatter(x=data[0], y=data[1], name='Data', mode='markers')
-        return go.Scatter3d(x=data[0], y=data[1], z=data[2], name='Data', mode='markers')
+            return go.Scatter(x=data[0], y=data[1], name='Data', mode='markers', marker={'size': 5, 'color': '#000000'})
+        return go.Scatter3d(x=data[0], y=data[1], z=data[2], name='Data', mode='markers', marker={'size': 3, 'color': '#000000'})
 
     @classmethod
-    def _box1d(cls, p1: List[int], p2: List[int]) -> go.Scatter:
-        return go.Scatter(x=[p1[0], p2[0]], y=[0, 0], mode='lines')
+    def _box1d(cls, p: List[int], q: List[int]) -> go.Scatter:
+        return go.Scatter(x=[p[0], q[0]], y=[0, 0], mode='lines')
 
     @classmethod
-    def _box2d(cls, p1: List[int], p2: List[int]) -> go.Scatter:
+    def _box2d(cls, p: List[int], q: List[int]) -> go.Scatter:
         x = []
         y = []
 
-        p_x = [p1[0], p2[0]]
-        p_y = [p1[1], p2[1]]
+        p_x = [p[0], q[0]]
+        p_y = [p[1], q[1]]
 
         x_path = [0, 1, 1, 0, 0]
         y_path = [0, 0, 1, 1, 0]
@@ -77,14 +78,14 @@ class Visualizer:
         return go.Scatter(x=x, y=y, mode='lines')
 
     @classmethod
-    def _box3d(cls, p1: List[int], p2: List[int]) -> go.Scatter3d:
+    def _box3d(cls, p: List[int], q: List[int]) -> go.Scatter3d:
         x = []
         y = []
         z = []
 
-        p_x = [p1[0], p2[0]]
-        p_y = [p1[1], p2[1]]
-        p_z = [p1[2], p2[2]]
+        p_x = [p[0], q[0]]
+        p_y = [p[1], q[1]]
+        p_z = [p[2], q[2]]
 
         x_path = [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, None, 1, 1, None, 1, 1, None, 0, 0]
         y_path = [0, 0, 1, 1, 0, 0, 0, 1, 1, 0, None, 0, 0, None, 1, 1, None, 1, 1]
@@ -107,39 +108,3 @@ class Visualizer:
                 z.append(p_z[z_path[i]])
 
         return go.Scatter3d(x=x, y=y, z=z, mode='lines')
-
-
-if __name__ == "__main__":
-    '''tree = RTree.create_in_memory(2, 1024, RTreeSplitType.BRUTE_FORCE)
-
-    root = Node(False, 10, (0, 0))
-    node1 = Node(True, 10, (0, 0))
-    node2 = Node(True, 10, (0, 0))
-    root.entries = [NonLeafEntry([0, 0], [1, 1], 1), NonLeafEntry([2, 0], [3, 2], 2)]
-    node1.entries = [LeafEntry([0, 1], 0), LeafEntry([1, 0], 1)]
-    node2.entries = [LeafEntry([2, 1], 2), LeafEntry([3, 1], 3)]
-
-    tree._storage.set_node(0, root)
-    tree._storage.add_node(node1)
-    tree._storage.add_node(node2)
-
-    Visualizer.visualize(tree)
-
-    '''
-    tree = RTree.create_in_file("soubor", 3, 1024, RTreeSplitType.QUADRATIC)
-
-    number_of_entries = 200
-
-    first_coord = [random.randint(0, 1000) for x in range(number_of_entries)]
-    second_coord = [random.randint(0, 1000) for x in range(number_of_entries)]
-    third_coord = [random.randint(0, 1000) for x in range(number_of_entries)]
-    for x, y, z, s in zip(first_coord, second_coord, third_coord, range(number_of_entries)):
-        if z % 10 == 0:
-            # Visualizer.visualize(tree)
-            # input()
-            pass
-        tree.insert([x, y, z], s)
-
-    print("Ahoj kamo")
-
-    Visualizer.visualize(tree)
