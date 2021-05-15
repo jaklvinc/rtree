b3443cadb9363ee477f19d0b597f751dcc0e7a60
Matouš Kulhan <kulhama7@fit.cvut.cz>
2021-05-16 01:08:08 +0200

    Finalize experiments

--- experiments.py
+++ experiments.py
@@ -5,85 +5,13 @@ import os
 import plotly.graph_objects as go
 from typing import Tuple, List, Any
 from rtree import RTree, RTreeSplitType
-from rtree.node import Node
-from rtree.storage import Storage, DiskStorage
-from visualization import Visualizer
-
-
-class StorageStatsDecorator(Storage):
-    def __init__(self, storage: Storage):
-        self._storage = storage
-        self._get_counter = 0
-        self._set_counter = 0
-        self._add_counter = 0
-
-    def stats_reset_get(self):
-        self._get_counter = 0
-
-    def stats_reset_set(self):
-        self._set_counter = 0
-
-    def stats_reset_add(self):
-        self._add_counter = 0
-
-    def stats_count_get(self) -> int:
-        return self._get_counter
-
-    def stats_count_set(self) -> int:
-        return self._set_counter
-
-    def stats_count_add(self) -> int:
-        return self._add_counter
-
-    def get_dim(self) -> int:
-        return self._storage.get_dim()
-
-    def get_node_size(self) -> int:
-        return self._storage.get_node_size()
-
-    def get_split_type(self) -> RTreeSplitType:
-        return self._storage.get_split_type()
-
-    def count(self) -> int:
-        return self._storage.count()
-
-    def get_node(self, index: int) -> Node:
-        self._get_counter += 1
-        return self._storage.get_node(index)
-
-    def set_node(self, index: int, node: Node):
-        self._set_counter += 1
-        return self._storage.set_node(index, node)
-
-    def add_node(self, node: Node) -> int:
-        self._add_counter += 1
-        return self._storage.add_node(node)
-
-
-class StatsDiskStorage(DiskStorage):
-    def __init__(self, filename: str):
-        super().__init__(filename)
-        self._write_counter = 0
-        self._read_counter = 0
-
-    def stats_reset_write(self):
-        self._write_counter = 0
-
-    def stats_reset_read(self):
-        self._read_counter = 0
-
-    def stats_count_write(self) -> int:
-        return self._write_counter
-
-    def stats_count_read(self) -> int:
-        return self._read_counter
 
 
 def time_fn(fn: callable, *args) -> Tuple[float, Any]:
-    start = time.perf_counter_ns()
+    start = time.perf_counter()
     value = fn(*args)
-    end = time.perf_counter_ns()
-    return (end - start) / 1000000000, value
+    end = time.perf_counter()
+    return (end - start), value
 
 
 def random_point(dim: int) -> List[int]:
@@ -172,46 +100,68 @@ def seq_search_knn(tree: RTree, point: List[int], k: int) -> List[Tuple[List[int
     return [x for _, x in queue]
 
 
-def insert_search_time(size: int, split_type: RTreeSplitType, data: List[Tuple[List[int], int]], boxes: List[Tuple[List[int], List[int]]], i_y: List[float], s_y: List[float]):
-    tree = RTree.create_in_file('./experiments/tmp.rtree', 2, size, split_type)
+def insert_search_time(dim: int, size: int, split_type: RTreeSplitType, data: List[Tuple[List[int], int]],
+                       boxes: List[Tuple[List[int], List[int]]], knn: List[Tuple[List[int], int]],
+                       i_y: List[float], s_r_y: List[float], s_k_y: List[float]):
+    tree = RTree.create_in_memory( dim, size, split_type)
     i_t, _ = time_fn(insert_data, tree, data)
     i_y.append(i_t)
 
-    s_t = 0
+    s_r_t = 0
     for box in boxes:
-        s, _ = time_fn(tree.search_range, box)
-        s_t += s
-    s_y.append(s_t)
+        t, _ = time_fn(tree.search_range, box)
+        s_r_t += t
+    s_r_y.append(s_r_t)
 
-    print(size, split_type.to_str())
+    s_k_t = 0
+    for p, k in knn:
+        t, _ = time_fn(tree.search_knn, p, k)
+        s_k_t += t
+    s_k_y.append(s_k_t)
 
 
-def insert_search_node_size_fig(x: List[int], bf: List[float], q: List[float], l: List[float], y_title: str) -> go.Figure:
+def insert_search_fig(x: List[int], bf: List[float], q: List[float], l: List[float], x_title: str, y_title: str) -> go.Figure:
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x, y=bf, name='Brute force split'))
     fig.add_trace(go.Scatter(x=x, y=q, name='Quadratic split'))
     fig.add_trace(go.Scatter(x=x, y=l, name='Linear split'))
     fig.update_layout(
-        xaxis_title='Node size [byte]',
+        xaxis_title=x_title,
         yaxis_title=y_title,
     )
-    fig.update_xaxes(tick0=128, dtick=128)
     fig.update_yaxes(tick0=0, dtick=0.5)
+    return fig
+
+
+def seq_search_fig(x: List[int], seq_y: List[float], tree_y: List[float], y_title: str):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=seq_y, name='Normal search'))
+    fig.add_trace(go.Scatter(x=x, y=tree_y, name='R-tree search'))
+    fig.update_layout(
+        xaxis_title='Number of data points',
+        yaxis_title=y_title,
+    )
+    fig.update_xaxes(tick0=100, dtick=100)
     return fig
 
 
 def experiment_insert_search_node_size():
     data = random_data(2, 1000)
     boxes = [random_box(2) for _ in range(200)]
+    knn = [(random_point(2), random.randint(5, 60)) for _ in range(20)]
 
     x = []
     bf_i_y = []
     q_i_y = []
     l_i_y = []
 
-    bf_s_y = []
-    q_s_y = []
-    l_s_y = []
+    bf_s_r_y = []
+    q_s_r_y = []
+    l_s_r_y = []
+
+    bf_s_k_y = []
+    q_s_k_y = []
+    l_s_k_y = []
 
     for size in range(128, 4097, 32):
         if size > 256 and size % 128 != 0:
@@ -220,47 +170,102 @@ def experiment_insert_search_node_size():
         x.append(size)
 
         if size <= 256:
-            insert_search_time(size, RTreeSplitType.BRUTE_FORCE, data, boxes, bf_i_y, bf_s_y)
+            insert_search_time(2, size, RTreeSplitType.BRUTE_FORCE, data, boxes, knn, bf_i_y, bf_s_r_y, bf_s_k_y)
 
-        insert_search_time(size, RTreeSplitType.QUADRATIC, data, boxes, q_i_y, q_s_y)
-        insert_search_time(size, RTreeSplitType.LINEAR, data, boxes, l_i_y, l_s_y)
+        insert_search_time(2, size, RTreeSplitType.QUADRATIC, data, boxes, knn, q_i_y, q_s_r_y, q_s_k_y)
+        insert_search_time(2, size, RTreeSplitType.LINEAR, data, boxes, knn, l_i_y, l_s_r_y, l_s_k_y)
+        print('  size {} B done'.format(size))
 
-    os.remove('./experiments/tmp.rtree')
+    fig = insert_search_fig(x, bf_i_y, q_i_y, l_i_y, 'Node size [byte]', 'Time to insert 1000 data points [s]')
+    fig.update_xaxes(tick0=128, dtick=128)
+    fig.write_html('./experiments/insert_node_size.html', auto_open=True)
 
-    fig_i = insert_search_node_size_fig(x, bf_i_y, q_i_y, l_i_y, 'Time to insert 1000 data points [s]')
-    fig_i.write_html('./experiments/insert_node_size.html', auto_open=True)
+    fig = insert_search_fig(x, bf_s_r_y, q_s_r_y, l_s_r_y, 'Node size [byte]', 'Time to do range search 200 times [s]')
+    fig.update_xaxes(tick0=128, dtick=128)
+    fig.update_yaxes(tick0=0, dtick=0.05)
+    fig.write_html('./experiments/search_range_node_size.html', auto_open=True)
 
-    fig_s = insert_search_node_size_fig(x, bf_s_y, q_s_y, l_s_y, 'Time to do range search 200 times [s]')
-    fig_s.write_html('./experiments/search_node_size.html', auto_open=True)
+    fig = insert_search_fig(x, bf_s_k_y, q_s_k_y, l_s_k_y, 'Node size [byte]', 'Time to do knn search 20 times [s]')
+    fig.update_xaxes(tick0=128, dtick=128)
+    fig.update_yaxes(tick0=0, dtick=0.1)
+    fig.write_html('./experiments/search_knn_node_size.html', auto_open=True)
 
 
-def experiment_seq():
-    tree = RTree.create_in_memory(2, 128, RTreeSplitType.QUADRATIC)
-    insert_data(tree, random_data(2, 1000))
+def experiment_insert_search_dimension():
+    data = random_data(10, 1000)
+    boxes = [random_box(10) for _ in range(200)]
+    knn = [(random_point(10), random.randint(5, 60)) for _ in range(20)]
 
     x = []
+    bf_i_y = []
+    q_i_y = []
+    l_i_y = []
+
+    bf_s_r_y = []
+    q_s_r_y = []
+    l_s_r_y = []
+
+    bf_s_k_y = []
+    q_s_k_y = []
+    l_s_k_y = []
+
+    for dim in range(1, 11):
+        x.append(dim)
+
+        data_dim = [([x[i] for i in range(dim)], d) for x, d in data]
+        boxes_dim = [([x[i] for i in range(dim)], [y[i] for i in range(dim)]) for x, y in boxes]
+        knn_dim = [([x[i] for i in range(dim)], k) for x, k in knn]
+
+        insert_search_time(2, 256, RTreeSplitType.BRUTE_FORCE, data_dim, boxes_dim, knn_dim, bf_i_y, bf_s_r_y, bf_s_k_y)
+        insert_search_time(2, 256, RTreeSplitType.QUADRATIC, data_dim, boxes_dim, knn_dim, q_i_y, q_s_r_y, q_s_k_y)
+        insert_search_time(2, 256, RTreeSplitType.LINEAR, data_dim, boxes_dim, knn_dim, l_i_y, l_s_r_y, l_s_k_y)
+        print('  dim {} done'.format(dim))
+
+    fig = insert_search_fig(x, bf_i_y, q_i_y, l_i_y, 'Dimension', 'Time to insert 1000 data points [s]')
+    fig.update_xaxes(tick0=1, dtick=1)
+    fig.update_yaxes(tick0=0.5, dtick=1)
+    fig.write_html('./experiments/insert_dimension.html', auto_open=True)
+
+    fig = insert_search_fig(x, bf_s_r_y, q_s_r_y, l_s_r_y, 'Dimension', 'Time to do range search 200 times [s]')
+    fig.update_xaxes(tick0=1, dtick=1)
+    fig.update_yaxes(tick0=0, dtick=0.05)
+    fig.write_html('./experiments/search_range_dimension.html', auto_open=True)
+
+    fig = insert_search_fig(x, bf_s_k_y, q_s_k_y, l_s_k_y, 'Dimension', 'Time to do knn search 20 times [s]')
+    fig.update_xaxes(tick0=1, dtick=1)
+    fig.update_yaxes(tick0=0, dtick=0.5)
+    fig.write_html('./experiments/search_knn_dimension.html', auto_open=True)
+
+
+def experiment_seq_search_data_len():
+    tree = RTree.create_in_memory(2, 128, RTreeSplitType.QUADRATIC)
+    boxes = [random_box(2) for _ in range(200)]
+
+    x = []
+
     seq_y = []
     tree_y = []
 
-    for i in range(50):
-        x.append(math.pow(i+1, 2))
+    for i in range(20):
+        insert_data(tree, random_data(2, 100))
+
+        x.append(i * 100 + 100)
+
         seq_sum = 0
         tree_sum = 0
-
-        for _ in range(100):
-            box = random_box_with_side_len(2, i+1)
-
+        for box in boxes:
             seq_time, _ = time_fn(seq_search_range, tree, box)
-            t_time, _ = time_fn(tree.search_range, box)
+            tree_time, _ = time_fn(tree.search_range, box)
 
             seq_sum += seq_time
-            tree_sum += t_time
-
+            tree_sum += tree_time
         seq_y.append(seq_sum)
         tree_y.append(tree_sum)
+        print('  {} data points done'.format(i * 100 + 100))
 
-    print("SEQ: ", seq_y)
-    print("TREE:", tree_y)
+    fig = seq_search_fig(x, seq_y, tree_y, 'Time to do range search 200 times [s]')
+    fig.update_yaxes(tick0=0, dtick=0.5)
+    fig.write_html('./experiments/seq_search_data_len.html', auto_open=True)
 
 
 def main():
@@ -268,7 +273,9 @@ def main():
         os.mkdir('./experiments')
 
     experiments = [
-        (experiment_insert_search_node_size, './experiments/insert_node_size.html, ./experiments/search_node_size.html')
+        (experiment_insert_search_node_size, './experiments/insert_node_size.html, ./experiments/search_range_node_size.html, ./experiments/search_knn_node_size.html'),
+        (experiment_insert_search_dimension, './experiments/insert_dimension.html, ./experiments/search_range_dimension.html, ./experiments/search_knn_dimension.html'),
+        (experiment_seq_search_data_len, './experiments/seq_search_data_len.html'),
     ]
 
     print('Generate:')
